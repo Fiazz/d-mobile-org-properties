fffcad82dc28d0d5d61d730f556827c14354c140
Fiazz <ismailfiaz@gmail.com>
2019-02-17 00:32:17 +0530

    Base Version

---
+++ src/main/java/com/d/api/dmobileorgproperties/DMobileOrgPropertiesApplication.java
@@ -0,0 +1,14 @@
+package com.d.api.dmobileorgproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMobileOrgPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMobileOrgPropertiesApplication.class, args);
+	}
+
+}
+
